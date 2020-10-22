88e2814a66a37bfc61f8b15a2f359ed420615551
Railey Hartheim <melancholia.raiven@gmail.com>
2020-10-22 20:48:15 +0500

    added some templates + functions in main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
-# here the http server will be
+from flask import Flask, render_template, request
+from quiz import Quiz
+app = Flask(__name__)
+
+quiz = Quiz()
+
+@app.route("/questions")
+def show_all_text():
+    return render_template('questions.html', questions=quiz.quiz_data)
+
+@app.route("/progress")
+def show_quiz():
+    return render_template('progress.html', questions=quiz.quiz_data)
 
 if __name__ == "__main__":
-    print("Hello world")
+    app.run()
